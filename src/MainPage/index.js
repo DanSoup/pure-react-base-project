e02c57527116531db411e4820278b2d8568551f9
DanSoup <dansoup@hotmail.co.uk>
2020-01-02 23:03:09 +0000

    pushing so I can access on desktop

---
+++ src/MainPage/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MainPage = (props) => {
+
+  return <React.Fragment><h1>
+      william ruusic
+    </h1>
+  </React.Fragment>
+
+}
+
+MainPage.propTypes = {
+
+};
+
+export default MainPage;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import './normalize.css';
 import './index.css';
+
+import MainPage from './MainPage'
 
 class App extends Component {
   render () {
-    return (
-      <div>Hello</div>
-    )
+    return <MainPage/>
   };
 }
 
