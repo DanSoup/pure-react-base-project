39a354188ed83e3d729b732acd5143f2aa1d8af7
Dan Soup <dansoup@hotmail.co.uk>
2022-03-11 16:39:09 +0000

    adding content to the site so it can be displayed

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -10,22 +10,28 @@ const MainPage = (props) => {
       <h1>Pure React Base Project</h1>
     </header>
     <nav>
-      <Link to="/path">PATH</Link>
+      <Link to="/">HOME</Link>
+      <Link to="/todo">TODO</Link>
     </nav>
-    <Switch location={location}>
-      <Route exact path="/">
-        
-      </Route>
-      <Route path="/path">
-        Path
-        <Route path="/path/blah">
-          Blah
+    <div id="content">
+      <Switch location={location}>
+        <Route exact path="/">
+          <p>This is my "pure react base project".</p>
+          <p>The idea behind this project was to create a React app without using create-react-app. As handy as create-react-app is I felt it was bloated with a lot of packages I was never going to useand so I created this.</p>
+          <p>From the ground up I wanted it to be lightweight but with things I'd be 99% likely to use in any project, such as Sass.</p>
+          <p>I clone this repository and update its details to create new projects, essentially it becomes a personal create-react-app.</p>
         </Route>
-      </Route>
-      <Route path="*">
-        404
-      </Route>
-    </Switch>
+        <Route path="/todo">
+          <ul>
+            <li>Configure Jest and add some default tests.</li>
+            <li>Come up with a better way to "clone" the project to create a new project.</li>
+          </ul>
+        </Route>
+        <Route path="*">
+          404
+        </Route>
+      </Switch>
+    </div>
   </>
 
 }
